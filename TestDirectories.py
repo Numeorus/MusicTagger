1a7caf9cd7759a4fe1ad7190729860385dcd2dbc
Sébastien Guichard <sebastien.guichard@viveris.fr>
2017-05-10 15:33:55 +0200

    ajout des slides explications/tuto tkinter

--- TestDirectories.py
+++ TestDirectories.py
@@ -1,7 +1,7 @@
 import taglib
 from os import listdir
 from os.path import isfile, join
-from Tkinter import Tk
+from Tkinter import *
 from tkFileDialog import askdirectory
 
 print "Test_programm"
@@ -10,15 +10,20 @@ print "Test_programm"
 Tk().withdraw() 
 mypath = askdirectory() 
 
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-print(onlyfiles)
+#onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+#print(onlyfiles)
+
+
+bouton=Button(fenetre, text="Fermer", command=fenetre.quit)
+bouton.pack()
 
 ## Github pytaglib
 # https://github.com/supermihi/pytaglib
 
 ## Tkinter pour l'interface graphique
 # http://apprendre-python.com/page-tkinter-interface-graphique-python-tutoriel
+# https://www.slideshare.net/r1chardj0n3s/tkinter-does-not-suck
 
 
 ## Voir pour modifier le filename du fichier
-## après les modifs dues au tags
+## apres les modifs dues au tags
